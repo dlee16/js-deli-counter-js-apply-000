9509746aedb2381c98d701dfb523e3848ae3f7d6
sensible-shell-8529 <diana.lee116@gmail.com>
2018-12-12 10:06:28 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var i = 0;
+var array(i);
+
+function takeANumber(line_no, name){
+array[i]= 
+
+//   var deliLine= [];{
+//   // for(var i = 0; i< line_no.length; i++){
+//   deliLine.push("Welcome, " + name + "."+ "You are number" + line_no + "in line.");}
+// } 
